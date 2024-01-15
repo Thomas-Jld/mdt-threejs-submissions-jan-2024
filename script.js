508553b26329480e067ee5ca36f01c4c9a29c938
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2024-01-15 12:06:24 +0100

    Randomizing currentMural and adding projects

--- script.js
+++ script.js
@@ -1,8 +1,141 @@
 
 const muralDiv = document.getElementById("mural");
-let currentMural = 0;
 
-function changeMural(title, author, source) {
+const muralList = [
+    {
+        title: "Aquapony",
+        author: "Violette A",
+        source: "https://violetteanicet.github.io/threejs_aquapony/",
+    },
+    {
+        title: "Turbocompressor",
+        author: "Augustin B",
+        source: "https://petitkiwi.github.io/BIOS-3D-Animation/"
+    },
+    {
+        title: "EEG Visualisation",
+        author: "Hippolyte B",
+        source: "https://hippobo.github.io/3js_eeg_viz/"
+    },
+    {
+        title: "Relaxing Waves",
+        author: "Diane B",
+        source: "https://dianubv.github.io/RelaxingWaves/"
+    },
+    {
+        title: "Candle",
+        author: "Gaëtan C",
+        source: "https://candle-drab.vercel.app/",
+        github: "https://github.com/GaetanCrd/Candle"
+    },
+    {
+        title: "Solar Oven",
+        author: "Yohann C",
+        source: "https://yocoss.github.io/SolarOven/"
+    },
+    {
+        title: "3D Speech-Driven Animation",
+        author: "Hugo Da",
+        source: "https://hugodmn.github.io/ThreeJsProject/"
+    },
+    {
+        title: "Thoughts in Motion",
+        author: "Hugo De",
+        source: "https://hdevoille.github.io/ThreeJS_MDT/",
+    },
+    {
+        title: "Art Life",
+        author: "Paul E",
+        source: "https://paul-even.github.io/threejs_class/"
+    },
+    {
+        title: "BIOS",
+        author: "Thaïs G",
+        source: "https://thaisgauthier.github.io/3D-projet/"
+    },
+    {
+        title: "Arctic Freezer",
+        author: "Gauthier G",
+        source: "https://jeeezzus.github.io/MDT_Three.JS/"
+    },
+    {
+        title: "Tiny Marsh",
+        author: "Noé G",
+        source: "https://antoinoe.github.io/threejs/"
+    },
+    {
+        title: "Plant Mural",
+        author: "Hugo J",
+        source: "https://hugo-jrlnd.github.io/MDT_project/"
+    },
+    {
+        title: "Drone Display",
+        author: "Nicolas L",
+        source: "https://nleboucher.github.io/DroneDiplay/",
+    },
+    {
+        title: "BIOS",
+        author: "Claire L",
+        source: "https://petitkiwi.github.io/BIOS-3D-Animation/"
+    },
+    {
+        title: "Bioreactor",
+        author: "Tom L",
+        source: "https://tooww.github.io/threejs-bioreactor/"
+    },
+    {
+        title: "GCode Viewer",
+        author: "Marc-Adrien M",
+        source: "https://ram6ces.github.io/gcode-viewer/"
+    },
+    {
+        title: "Luma Visualizer",
+        author: "Claudio O",
+        source: "https://claudio9701.github.io/ift-mdt-three/"
+    },
+    {
+        title: "Point Cloud Visualisation",
+        author: "Etienne P",
+        source: "https://etienne248.github.io/TreeJs_Point_Cloud_visualisation/"
+    },
+    {
+        title: "HeartBit",
+        author: "Marine R",
+        source: "https://marinereynaud25.github.io/HeartBit-ThreeJS/"
+    },
+    {
+        title: "Internet of Plants",
+        author: "Matthieu S",
+        source: "https://matthieu-sgi.github.io/Internet-of-Plants-threejs/"
+    },
+    {
+        title: "Personal Portfolio",
+        author: "Nicolas S",
+        source: "https://stasnicolas.com/",
+        github: "https://github.com/COLVERTYETY/portfolio",
+    },
+    {
+        title: "Whistle Babe",
+        author: "Mathis T",
+        source: "https://matlamenasse.github.io/whistle_babe/"
+    },
+    {
+        title: "Voice Interaction",
+        author: "Nathan V",
+        source: "https://arcadia24.github.io/voice-interaction-three/"
+    },
+]
+
+let currentMural = Math.floor(Math.random() * muralList.length);
+
+if (window.location.hash) {
+    let hash = window.location.hash.substring(1);
+    currentMural = parseInt(hash);
+}
+
+console.log(currentMural);
+
+function changeMural(title, author, source, hash) {
     let titleElement = document.getElementById("title");
     titleElement.className = "muralTitle";
     titleElement.innerHTML = title;
@@ -14,108 +147,11 @@ function changeMural(title, author, source) {
     let iframeElement = document.getElementById("muralIframe");
     iframeElement.src = source;
 
-    window.history.pushState("", "", `#${currentMural}`);
-    window.location.hash = currentMural;
+    window.history.pushState("", "", `#${hash}`);
+    window.location.hash = hash;
 }
 
-let muralList = [
-    {
-        title: "Arctic Freezer",
-        author: "Gauthier G",
-        source: "https://jeeezzus.github.io/MDT_Three.JS/"
-    },
-    {
-        title: "Solar Oven",
-        author: "Yohann C",
-        source: "https://yocoss.github.io/SolarOven/"
-    },
-    {
-        title: "Luma Visualizer",
-        author: "Claudio O",
-        source: "https://claudio9701.github.io/ift-mdt-three/"
-    },
-    {
-        title: "3D Speech-Driven Animation",
-        author: "Hugo Da",
-        source: "https://hugodmn.github.io/ThreeJsProject/"
-    },
-    {
-        title: "BIOS",
-        author: "Thaïs G",
-        source: "https://thaisgauthier.github.io/3D-projet/"
-    },
-    {
-        title: "GCode Viewer",
-        author: "Marc-Adrien M",
-        source: "https://ram6ces.github.io/gcode-viewer/"
-    },
-    {
-        title: "Whistle Babe",
-        author: "Mathis T",
-        source: "https://matlamenasse.github.io/whistle_babe/"
-    },
-    {
-        title: "Tiny Marsh",
-        author: "Noé G",
-        source: "https://antoinoe.github.io/threejs/"
-    },
-    {
-        title: "Relaxing Waves",
-        author: "Diane B",
-        source: "https://dianubv.github.io/RelaxingWaves/"
-    },
-    {
-        title: "Voice Interaction",
-        author: "Nathan V",
-        source: "https://arcadia24.github.io/voice-interaction-three/"
-    },
-    {
-        title: "HeartBit",
-        author: "Marine R",
-        source: "https://marinereynaud25.github.io/HeartBit-ThreeJS/"
-    },
-    {
-        title: "Point Cloud Visualisation",
-        author: "Etienne P",
-        source: "https://etienne248.github.io/TreeJs_Point_Cloud_visualisation/"
-    },
-    {
-        title: "Plant Mural",
-        author: "Hugo J",
-        source: "https://hugo-jrlnd.github.io/MDT_project/"
-    },
-    {
-        title: "Internet of Plants",
-        author: "Matthieu S",
-        source: "https://matthieu-sgi.github.io/Internet-of-Plants-threejs/"
-    },
-    {
-        title: "EEG Visualisation",
-        author: "Hippolyte B",
-        source: "https://hippobo.github.io/3js_eeg_viz/"
-    },
-    {
-        title: "BIOS",
-        author: "Claire L",
-        source: "https://petitkiwi.github.io/BIOS-3D-Animation/"
-    },
-    {
-        title: "Turbocompressor",
-        author: "Augustin B",
-        source: "https://petitkiwi.github.io/BIOS-3D-Animation/"
-    },
-    {
-        title: "Candle",
-        author: "Gaëtan C",
-        source: "https://candle-drab.vercel.app/",
-        github: "https://github.com/GaetanCrd/Candle"
-    },
-    {
-        title: "Thoughts in Motion",
-        author: "Hugo De",
-        source: "https://hdevoille.github.io/ThreeJS_MDT/",
-    }
-]
+
 
 
 let viewCode = document.getElementById("muralVisitButton");
@@ -144,21 +180,17 @@ function triggerAnimation() {
 let nextButton = document.getElementById("muralNextButton");
 nextButton.onclick = () => {
     currentMural = (currentMural + 1) % muralList.length;
-    changeMural(muralList[currentMural].title, muralList[currentMural].author, muralList[currentMural].source);
+    changeMural(muralList[currentMural].title, muralList[currentMural].author, muralList[currentMural].source, currentMural);
     triggerAnimation();
 }
 
 let previousButton = document.getElementById("muralPreviousButton");
 previousButton.onclick = () => {
     currentMural = (currentMural - 1 + muralList.length) % muralList.length;
-    changeMural(muralList[currentMural].title, muralList[currentMural].author, muralList[currentMural].source);
+    changeMural(muralList[currentMural].title, muralList[currentMural].author, muralList[currentMural].source, currentMural);
     triggerAnimation();
 }
 
 
-if (window.location.hash) {
-    let hash = window.location.hash.substring(1);
-    currentMural = parseInt(hash);
-}
 
-changeMural(muralList[currentMural].title, muralList[currentMural].author, muralList[currentMural].source);
+changeMural(muralList[currentMural].title, muralList[currentMural].author, muralList[currentMural].source, currentMural);
